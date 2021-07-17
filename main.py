3cbd0207dc03b4f2abb12a564985c07033e5ee0f
Akhil Gosala <60161763+Akhildotexe@users.noreply.github.com>
2021-07-17 11:03:02 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+num1= float(input("Enter the first Number format #+x=#: "))
+
+op = input("Enter the operator: ")
+
+num2= float(input("Enter the 3nd Number #+x=#: "))
+
+if op =="+" :
+    print(f'x+{(num1)} = {(num2)}  x = {(num2 - num1)}')
+
+if op == "-":
+    print(f'x-{(num1)} = {(num2)}  x = {(num2 + num1)}')
+else 
+    print("pls redo your equation.")
