4e1f2c6fbda6ac84db403c9ac199b77dcbcd0a57
Akhil Gosala <60161763+Akhildotexe@users.noreply.github.com>
2022-05-12 19:50:51 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,20 +1,20 @@
-num1= float(input("Enter the first Number format #+x=#: "))
+num1= float(input("Enter the first Number format # + x = # > "))
 
 op = input("Enter the operator: ")
 
-num2= float(input("Enter the 3nd Number #+x=#: "))
+num2= float(input("Enter the 3nd Number #+x=# >  "))
 
 if op =="+" :
-    print(f'x+{(num1)} = {(num2)}  x = {(num2 - num1)}')
+    print(f'x+{num1} = {num2}  x = {num2 + num1}')
 
 if op == "-":
-    print(f'x-{(num1)} = {(num2)}  x = {(num2 + num1)}')
+    print(f'x-{num1} = {num2}  x = {num2 - num1}')
     
 if op == "*" : 
-    print(f'x*{(num1)} = {(num2)}  x = {(num2 / num1)}') 
+    print(f'x*{num1} = {num2}  x = {num2 / num1}') 
 
-if op == "/" : 
-    print(f'x/{(num1)} = {(num2)}  x = {(num2 * num1)}')    
+if op == "/" :
+    print(f'x/{num1} = {num2}  x = {num2 * num1}')    
 
-else 
+else:
     print("pls redo your equation.")
