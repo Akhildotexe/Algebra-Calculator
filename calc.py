287167316b9700644686397dfdef0e2ffab977ff
Akhil Gosala <60161763+Akhildotexe@users.noreply.github.com>
2022-05-13 09:17:29 -0400

    made cleaner

--- calc.py
+++ calc.py
@@ -1,20 +1,19 @@
-num1= float(input("Enter the first Number format # + x = # > "))
+num1= float(input("Enter the first Number format # + x = #: "))
 
 op = input("Enter the operator: ")
 
-num2= float(input("Enter the 3nd Number #+x=# >  "))
+num2= float(input("Enter the 3nd Number # + x = #: "))
 
-if op =="+" :
-    print(f'x+{num1} = {num2}  x = {num2 + num1}')
+answer = 0
+match op:
+    case "+":
+        answer = num2 - num1
+    case "-":
+        answer = num2 + num1
+    case "*":
+        answer = num2 / num1
+    case "/":
+        answer = num2 * num1
 
-if op == "-":
-    print(f'x-{num1} = {num2}  x = {num2 - num1}')
-    
-if op == "*" : 
-    print(f'x*{num1} = {num2}  x = {num2 / num1}') 
-
-if op == "/" :
-    print(f'x/{num1} = {num2}  x = {num2 * num1}')    
-
-else:
-    print("pls redo your equation.")
+        raise ValueError("Invalid Operator.")
+print(f"x = {answer}")
